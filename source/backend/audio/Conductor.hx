6da01b7df01bb3039673ed94c876f1b3806cf962
Nebula S. Nova <jasontheone111@outlook.com>
2025-06-02 21:40:45 -0400

    idk shit or something

---
+++ source/backend/audio/Conductor.hx
@@ -0,0 +1,22 @@
+package backend;
+
+import flixel.FlxG;
+
+class Conductor {
+
+	public static var bpm(default, null):Float = 100;
+
+	public static var curStep(default, null):Int = 0;
+	
+	public static var curBeat(default, null):Int = 0;
+
+	public static var curMeasure(default, null):Int = 0;
+
+	public static function init() {
+		FlxG.signals.preUpdate.add(update);
+	}
+
+	public static function update() {
+		
+	}
+}
