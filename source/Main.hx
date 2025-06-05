3b7ddc728dcb97a2f1c54c211ef97b2ea55e0fb1
Nebula S. Nova <jasontheone111@outlook.com>
2025-06-05 15:26:49 -0400

    dropped to 720

--- source/Main.hx
+++ source/Main.hx
@@ -9,6 +9,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(1920, 1080, PlayState, 60, 60, true, false));
+		addChild(new FlxGame(1280, 720, PlayState, 60, 60, true, false));
 	}
 }
