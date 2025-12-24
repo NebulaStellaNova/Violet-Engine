cce2dc3e9745bbabf194bba28e392af0694a3160
Viwu <sinconsistencia@gmail.com>
2025-12-24 08:39:52 -0600

    creditObjectMaxY

--- source/violet/states/menus/CreditsMenu.hx
+++ source/violet/states/menus/CreditsMenu.hx
@@ -13,6 +13,8 @@ class CreditsMenu extends violet.backend.SubStateBackend {
 
 	public var creditObjects:FlxTypedGroup<FlxSprite>;
 
+	var creditObjectMaxY = 0.0;
+
 	override function create() {
 		super.create();
 
@@ -25,12 +27,11 @@ class CreditsMenu extends violet.backend.SubStateBackend {
 			trace(e.message);
 		}
 
-		var y = 0.0;
 		for (credit in creditsJSON.credits) {
 			var title:NovaText = new NovaText(0, 0, 0, credit.title, 32);
 
-			title.y = y;
-			y += title.height * 1.1;
+			title.y = creditObjectMaxY;
+			creditObjectMaxY += title.height * 1.1;
 
 			creditObjects.add(title);
 
@@ -40,13 +41,15 @@ class CreditsMenu extends violet.backend.SubStateBackend {
 				if (contrib.role != null)
 					contribText.text += ' : ${contrib.role}';
 
-				contribText.y = y;
-				y += contribText.height;
+				contribText.y = creditObjectMaxY;
+				creditObjectMaxY += contribText.height;
 
 				creditObjects.add(contribText);
 
 				if (contrib.icon != null || contrib.https_icon != null) {
-					var contribIcon:NovaSprite = new NovaSprite(0, y);
+					var contribIcon:NovaSprite = new NovaSprite(0, creditObjectMaxY);
+
+					creditObjectMaxY += contribIcon.height;
 
 					if (contrib.icon != null && contrib.https_icon == null)
 						contribIcon.loadSprite(Paths.image(contrib.icon, 'menus/creditsmenu'));
@@ -60,8 +63,13 @@ class CreditsMenu extends violet.backend.SubStateBackend {
 					creditObjects.add(contribIcon);
 
 					contribText.x += contribIcon.width * 1.1;
+					contribText.y += contribIcon.height / 2;
 				}
 			}
+		}
+
+		for (obj in creditObjects.members) {
+			obj.scrollFactor.set();
 		}
 
 		trace('menuedCredits');
@@ -73,10 +81,9 @@ class CreditsMenu extends violet.backend.SubStateBackend {
 		for (obj in creditObjects.members) {
 			obj.y -= obj.height / 4;
 
-            if (obj.y < FlxG.camera.y - obj.height * 2)
-            {
-                obj.y = FlxG.camera.y + FlxG.height;
-            }
+			if (obj.y < FlxG.camera.y - obj.height * 2) {
+				obj.y = creditObjectMaxY;
+			}
 		}
 	}
 }
