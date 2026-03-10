d5e789b470d3630d6d7743b163160d9b0a1333c7
Nebula S. Nova <jasontheone111@outlook.com>
2026-03-10 14:34:21 -0400

    bye bye hmm fuck you

---
+++ install-libraries.bat
@@ -0,0 +1,27 @@
+mkdir .haxelib
+haxelib fixrepo
+@REM INSTALL HAXELIB LIBS
+haxelib install lime
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-addons
+haxelib install flixel-animate
+haxelib install haxeui-core
+haxelib install haxeui-flixel
+haxelib install hxdiscord_rpc
+haxelib install hxp
+haxelib install hxvlc
+haxelib install hxWindowColorMode
+haxelib install moonchart
+haxelib install thx.core
+haxelib install thx.semver
+haxelib install yaml
+@REM INSTALL GIT LIBS
+haxelib git flxrhythmconductor https://github.com/PurSnake/FlxRhythmConductor
+haxelib git hxcpp https://github.com/FunkinCrew/hxcpp
+haxelib git hxcpp-debug-server https://github.com/FunkinCrew/hxcpp-debugger
+haxelib git hxhardware https://github.com/Vortex2Oblivion/hxhardware
+haxelib git json2object https://github.com/FunkinCrew/json2object
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib git lscript https://github.com/SrtHero278/lscript
+haxelib git rulescript https://github.com/Kriptel/RuleScript dev
