fd27ec3a61a403646cb8e85442f8bb3d09f2b092
Nebula S. Nova <jasontheone111@outlook.com>
2026-03-14 15:51:58 -0400

    Fix stages loading all every stage script if null

--- source/violet/data/chart/ChartRegistry.hx
+++ source/violet/data/chart/ChartRegistry.hx
@@ -46,6 +46,7 @@ class ChartRegistry {
 		if (chartDatas.exists(id)) return chartDatas.get(id);
 
 		var data = ChartConverters.convertChart(chartCache.get(id));
+		data.stage ??= "mainStage"; // Prevent bug where if no stage is assigned it will load all stage's scripts.
 		chartDatas.set(id, data);
 		return data;
 	}
