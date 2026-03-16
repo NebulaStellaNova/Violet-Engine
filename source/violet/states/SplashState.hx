5586b42fe525b8129d1fe8bbbc11e056a6a44814
Nebula S. Nova <jasontheone111@outlook.com>
2026-03-16 05:06:01 -0400

    bye bye RR boot

--- source/violet/states/SplashState.hx
+++ source/violet/states/SplashState.hx
@@ -8,38 +8,38 @@ class SplashState extends violet.backend.StateBackend {
 
     override public function create() {
 		super.create();
-
-        haxeLogo = new NovaSprite(225, 30, Paths.image('menus/splashscreen/haxe'));
-        haxeLogo.animation.addByPrefix('intro', 's', 24, false);
+        //
+        //
+        //
+        //
+        //
+        //
+        //                    Rodney, Please make this embed
+        //                                               ||
+        //                                               ||
+        //                                               ||
+        //                                               ||
+        //                                               ||
+        //                                               ||
+        //                                               vv
+        haxeLogo = new NovaSprite(225, 30, Paths.image('boot'));
+        haxeLogo.animation.addByPrefix('intro', 'flixel', 24, false);
         haxeLogo.animation.play('intro', true);
-        haxeLogo.animation.onFinish.add((_)->flixel.tweens.FlxTween.tween(haxeLogo, { alpha: 0 }, 1, { onComplete: (_)->{
-            intro.visible = true;
-            intro.animation.play('intro', true);
-            intro.screenCenter();
-        }}));
-        add(haxeLogo);
-
-        intro = new NovaSprite(0, 0, Paths.image('menus/splashscreen/intro'));
-        intro.animation.addByPrefix('intro', 'a', 24, false);
-        intro.animation.addByIndices('loop', 'a', [124, 125, 126, 127], null, 24, true);
-        intro.animation.onFinish.add((name)->{
-            if (name != 'intro') return;
-            intro.animation.play('loop', true);
-            flixel.tweens.FlxTween.tween(intro, { alpha: 0 }, 1, { onComplete: (_)->{
-                FlxG.switchState(TitleState.new);
-            }});
+        haxeLogo.updateHitbox();
+        haxeLogo.screenCenter();
+        haxeLogo.animation.onFinish.addOnce((n)->{
+            FlxG.switchState(TitleState.new);
         });
-        intro.visible = false;
-        add(intro);
+        add(haxeLogo);
     }
 
     override public function update(elapsed:Float) {
         super.update(elapsed);
 
         // `animation` is nullable.
-        if (haxeLogo.animation?.curAnim?.curFrame == 36 && !playedSound) {
+        if (haxeLogo.animation?.curAnim?.curFrame >= 1 && !playedSound) {
             playedSound = true;
-            FlxG.sound.play(Cache.sound('haxeIntro'));
+            FlxG.sound.play(Paths.sound('flixel'));
         }
 
         if (Controls.accept) {
