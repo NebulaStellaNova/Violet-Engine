e4744b37870f71de2a688250c1424af096206995
Nebula S. Nova <jasontheone111@outlook.com>
2026-03-21 22:44:48 -0400

    Rework macro a lil

--- source/violet/backend/VarTweenMacro.hx
+++ source/violet/backend/VarTweenMacro.hx
@@ -1,15 +1,23 @@
 package violet.backend;
 
 #if macro
-import haxe.macro.Compiler;
 import haxe.macro.Context;
 import haxe.macro.Expr;
 
 
 class VarTweenMacro {
+    public static var macroContext:Array<Field>;
+
+    public static var tracePos = macro {
+        methodName: 'tween',
+        className: 'FlxTween',
+        fileName: 'FlxTween.hx',
+        lineNumber: 985
+    }
+
     public static macro function init():Array<Field> {
-		var classFields = Context.getBuildFields();
-        classFields = setExpr(classFields, 'update', macro {
+		macroContext = Context.getBuildFields();
+        setExpr('update', macro {
             var delay:Float = (executions > 0) ? loopDelay : startDelay;
 
             // Leave properties alone until delay is over
@@ -29,26 +37,16 @@ class VarTweenMacro {
                         else this.cancel();
             }
         });
-        classFields = setExpr(classFields, 'setStartValues', macro {
+        setExpr('setStartValues', macro {
             for (info in _propertyInfos)
             {
                 if (Reflect.getProperty(info.object, info.field) == null)
-                    violet.backend.console.Logs.traceCallback('error:The object does not have the property "${info.field}", cancelling tween.', {
-                        methodName: 'tween',
-                        className: 'FlxTween',
-                        fileName: 'FlxTween.hx',
-                        lineNumber: 985
-                    });
+                    violet.backend.console.Logs.traceCallback('error:The object does not have the property "${info.field}", cancelling tween.', $tracePos);
 
                     var value:Dynamic = Reflect.getProperty(info.object, info.field);
                     if (Math.isNaN(value)) {
                         this.cancel();
-                        violet.backend.console.Logs.traceCallback('error:The property "${info.field}" is not numeric.', {
-                            methodName: 'tween',
-                            className: 'FlxTween',
-                            fileName: 'FlxTween.hx',
-                            lineNumber: 985
-                        });
+                        violet.backend.console.Logs.traceCallback('error:The property "${info.field}" is not numeric.', $tracePos);
                         break;
                     }
 
@@ -56,17 +54,12 @@ class VarTweenMacro {
                     info.range = info.range - value;
             }
         });
-        classFields = setExpr(classFields, 'initializeVars', macro {
+        setExpr('initializeVars', macro {
             var fieldPaths:Array<String> = [];
             if (Reflect.isObject(_properties))
                 fieldPaths = Reflect.fields(_properties);
             else
-                violet.backend.console.Logs.traceCallback("error:Unsupported properties container - use an object containing key/value pairs.", {
-                    methodName: 'tween',
-                    className: 'FlxTween',
-                    fileName: 'FlxTween.hx',
-                    lineNumber: 985
-                });
+                violet.backend.console.Logs.traceCallback("error:Unsupported properties container - use an object containing key/value pairs.", $tracePos);
 
             for (fieldPath in fieldPaths)
             {
@@ -77,12 +70,7 @@ class VarTweenMacro {
                 {
                     target = Reflect.getProperty(target, component);
                     if (!Reflect.isObject(target))
-                        violet.backend.console.Logs.traceCallback('error:The object does not have the property "$component" in "$fieldPath"', {
-                            methodName: 'tween',
-                            className: 'FlxTween',
-                            fileName: 'FlxTween.hx',
-                            lineNumber: 985
-                        });
+                        violet.backend.console.Logs.traceCallback('error:The object does not have the property "$component" in "$fieldPath"', $tracePos);
                 }
 
                 _propertyInfos.push({
@@ -93,18 +81,17 @@ class VarTweenMacro {
                 });
             }
         });
-		return classFields;
+		return macroContext;
 	}
 
-    public static function setExpr(fields:Array<Field>, fieldName:String, expr:Expr):Array<Field> {
-        var func = fields.filter(field -> return field.name == fieldName)[0];
+    public static function setExpr(fieldName:String, expr:Expr) {
+        var func = macroContext.filter(field -> return field.name == fieldName)[0];
 		switch (func.kind) {
 			case FFun(f):
                 f.expr = expr;
 				func.kind = FFun(f);
 			default:
         }
-        return fields;
     }
 }
 
