0446f2952baea0ce7bd7b14baee0ca4c36729f0d
Nebula S. Nova <jasontheone111@outlook.com>
2026-03-24 03:27:02 -0400

    Add borderless fullscreen keybind

--- source/violet/states/InitialState.hx
+++ source/violet/states/InitialState.hx
@@ -4,11 +4,21 @@ import lemonui.utils.MathUtil;
 import violet.states.menus.OptionsMenu;
 import violet.backend.StateBackend;
 import violet.backend.audio.Conductor;
+import openfl.system.Capabilities;
 
 class InitialState extends StateBackend { // for now
 
 	var logo:NovaSprite = new NovaSprite(Paths.image("icons/dad"));
 	var loadingBar:NovaSprite;
+
+	public static var fullscreen:Bool = false;
+
+	public static var defaultParams = {
+		x: 0,
+		y: 0,
+		width: 0,
+		height: 0
+	}
 
 	public static var loadingPercent:Float = 0;
 
@@ -16,6 +26,11 @@ class InitialState extends StateBackend { // for now
 		FlxG.fixedTimestep = false;
 		FlxSprite.defaultAntialiasing = true; // this ain't a pixel game... yeah ik week 6 exists!
 		FlxG.cameras.useBufferLocking = true;
+
+		defaultParams.x = lime.app.Application.current.window.x;
+		defaultParams.y = lime.app.Application.current.window.y;
+		defaultParams.width = lime.app.Application.current.window.width;
+		defaultParams.height = lime.app.Application.current.window.height;
 
 		super.create();
 
@@ -28,14 +43,43 @@ class InitialState extends StateBackend { // for now
 		// for later use
 		#end
 
-		FlxG.signals.preUpdate.add(() -> {
-			if (OptionsMenu.instance != null)
-				if (!OptionsMenu.instance.canSelectMenu) return;
-
+		FlxG.signals.postStateSwitch.add(()->{
 			if (!Std.isOfType(FlxG.state, PlayState)) {
 				PlayState.hasSeenCutscene = false;
 				PlayState.isStoryMode = false;
+				PlayState.playlist = [];
 			}
+		});
+
+		FlxG.signals.preUpdate.add(() -> {
+
+			if (OptionsMenu.instance != null)
+				if (!OptionsMenu.instance.canSelectMenu) return;
+
+			if (Controls.fullscreen) {
+				fullscreen = !fullscreen;
+				if (fullscreen) {
+					lime.app.Application.current.window.borderless = true;
+					lime.app.Application.current.window.resize(Math.round(Capabilities.screenResolutionX), Math.round(Capabilities.screenResolutionY)+8);
+					lime.app.Application.current.window.x = 0;
+					lime.app.Application.current.window.y = -4;
+				} else {
+					lime.app.Application.current.window.borderless = false;
+					lime.app.Application.current.window.resize(defaultParams.width, defaultParams.height);
+					lime.app.Application.current.window.x = defaultParams.x;
+					lime.app.Application.current.window.y = defaultParams.y;
+				}
+			}
+
+			if (Controls.reloadGame) {
+				Conductor.pause();
+				ModdingAPI.reloadRegistries();
+				FlxG.resetState();
+			}
+			if (Controls.resetState)
+				FlxG.resetState();
+			if (Controls.shortcutState)
+				FlxG.switchState(() -> new violet.states.menus.MainMenu());
 		});
 
 		loadingBar = new NovaSprite().makeGraphic(FlxG.width * 0.9, 20);
