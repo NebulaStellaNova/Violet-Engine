25285307769e0daf1c52159c832c8475f5497bd6
Nebula S. Nova <jasontheone111@outlook.com>
2026-03-30 10:38:17 -0400

    extra zip funcy

--- source/violet/backend/filesystem/ModdingAPI.hx
+++ source/violet/backend/filesystem/ModdingAPI.hx
@@ -84,7 +84,10 @@ class ModdingAPI {
 				FileSystem.createDirectory(modPath);
       			Sys.command("attrib +h " + modPath);
 
+				#if debug var startTime = Sys.time(); #end
 				violet.backend.utils.ZipUtil.extractZip('$MOD_FOLDER/$path', modPath);
+				#if debug var delta = (Sys.time() - startTime) * 1000;
+				trace('debug:VMod extraction took ${Math.round(delta*100)/100} milliseconds');
 			}
 		}
 
