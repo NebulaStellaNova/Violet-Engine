477c55dec482517e77a3bc8dbe4238e7873cd1cc
Nebula S. Nova <jasontheone111@outlook.com>
2026-04-07 10:02:20 -0400

    ?

--- setup.bat
+++ setup.bat
@@ -4,4 +4,5 @@ haxelib fixrepo
 haxelib install hxp
 haxelib install lime
 haxelib git hxcpp https://github.com/FunkinCrew/hxcpp
+haxelib git hxcpp https://github.com/FunkinCrew/hxcpp
 haxelib git hxcpp-debug-server https://github.com/FunkinCrew/hxcpp-debugger
