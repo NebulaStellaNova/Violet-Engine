81fae7c99cf611f08a242cd011aa5e84bfbd454b
Nebula S. Nova <jasontheone111@outlook.com>
2026-04-07 10:55:03 -0400

    mutha fucka

--- setup.bat
+++ setup.bat
@@ -4,7 +4,10 @@ haxelib fixrepo
 haxelib install hxp
 haxelib install lime
 haxelib install openfl
-haxelib run lime setup
-haxelib run openfl setup
 haxelib git hxcpp https://github.com/FunkinCrew/hxcpp
 haxelib git hxcpp-debug-server https://github.com/FunkinCrew/hxcpp-debugger
+
+haxelib remove lime
+haxelib install lime
+haxelib run lime setup
+haxelib run openfl setup
