6c3f5d7c50e6ac9607ab583fbff39815271feffd
Nebula S. Nova <jasontheone111@outlook.com>
2026-04-09 14:57:44 -0400

    Oops x2

--- source/violet/states/ModSubState.hx
+++ source/violet/states/ModSubState.hx
@@ -2,7 +2,7 @@ package violet.states;
 
 import violet.backend.SubStateBackend;
 
-class ModState extends SubStateBackend {
+class ModSubState extends SubStateBackend {
 
     public var id:String;
     public var args:Dynamic;
