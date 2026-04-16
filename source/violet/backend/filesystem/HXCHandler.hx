88f207480de9d4e6f0ef518f682572c6e3bd0a33
Nebula S. Nova <jasontheone111@outlook.com>
2026-04-16 13:58:56 -0400

    [REMOVAL] Removed unused code I forgot about

--- source/violet/backend/filesystem/HXCHandler.hx
+++ source/violet/backend/filesystem/HXCHandler.hx
@@ -9,10 +9,6 @@ class HXCHandler extends flixel.FlxBasic {
 
 	public static var instance:HXCHandler;
 
-	public static var importRedirects:Map<String, String> = [
-		'funkin.modding.module.Module' => 'violet.backend.scripting.hxc.Module'
-	];
-
 	public var hxcScripts:ScriptPack = new ScriptPack();
 
 	override public function new() {
@@ -21,12 +17,7 @@ class HXCHandler extends flixel.FlxBasic {
 	}
 
 	public function addScript(path:String) {
-		var scriptCode:String = FileUtil.getFileContent(path);
-		for (i in importRedirects.keys())
-			scriptCode = scriptCode.replace(i, importRedirects.get(i));
-
 		var script = new violet.backend.scripting.ModuleScript(path);
-		var filePath = path.split("/");
 		hxcScripts.addScript(script);
 	}
 
