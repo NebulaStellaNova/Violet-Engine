94b1a351633a5de75192892b6e8b722d9fa9820c
Nebula S. Nova <jasontheone111@outlook.com>
2026-05-02 17:02:37 -0400

    WiP cocoa and eggnot re-import

---
+++ assets/data/notetypes/mom.hx
@@ -0,0 +1,11 @@
+function noteHit(event) {
+    if (event.noteType == "mom") {
+        event.animationSuffix = "alt";
+    }
+}
+
+function sustainHit(event) {
+    if (event.noteType == "mom") {
+        event.animationSuffix = "alt";
+    }
+}
