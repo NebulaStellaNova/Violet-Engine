baaca3582c7783e37a484737002c5c912dd2fc1d
Nebula S. Nova <jasontheone111@outlook.com>
2026-05-05 21:10:37 -0400

    Made vmod extraction less scary
    # Console no longer pops up for a frame for every vmod

--- source/violet/backend/utils/ZipUtil.hx
+++ source/violet/backend/utils/ZipUtil.hx
@@ -1,24 +1,38 @@
 package violet.backend.utils;
 
+import flixel.graphics.FlxGraphic;
+import openfl.display.BitmapData;
+import haxe.zip.Reader;
+import haxe.io.Path;
+
 class ZipUtil {
 
 	/**
 	 * Near instantaneous zip extracting via sys commands :D
 	 */
 	public static function extractZip(inputPath:String, outputPath:String):Void {
+		var process = null;
 		#if windows
-		var process = Sys.command("tar", ["-xf", inputPath, "-C", outputPath]);
+		process = new sys.io.Process("tar", ["-xf", inputPath, "-C", outputPath]);
 		#else
-		var process = Sys.command("unzip", [inputPath, "-d", outputPath]);
+		process = new sys.io.Process("unzip", [inputPath, "-d", outputPath]);
 		#end
+		var output = process.stdout.readAll().toString();
+		var errors = process.stderr.readAll().toString();
+		var code = process.exitCode();
+		process.close();
 	}
 
 	public static function getZipEntries(inputPath:String):Array<String> {
 		#if windows
-		Sys.command('tar -tf "$inputPath" > output.txt');
-		var entries = FileUtil.getFileContent('output.txt').split('\n').filter(s -> s != "");
+		var process = new sys.io.Process('tar -tf "$inputPath"');
+		var output = process.stdout.readAll().toString();
+		var errors = process.stderr.readAll().toString();
+		var code = process.exitCode();
+		process.close();
+
+		var entries = output.split('\n').filter(s -> s != "");
 		for (i in 0...entries.length) entries[i] = entries[i].trim();
-		sys.FileSystem.deleteFile('output.txt');
 		return entries;
 		#else
 		// The echo for this on mac and linux is like SUPER extra and I don't feel like dealing with allat so someone else figure it out.
