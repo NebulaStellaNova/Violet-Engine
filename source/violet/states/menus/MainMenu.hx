263c6e4cce96b10117578e8194ec86a36f44b28c
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-05-19 23:06:41 -0400

    works, only looks right with 6 items atm

--- source/violet/states/menus/MainMenu.hx
+++ source/violet/states/menus/MainMenu.hx
@@ -1,6 +1,9 @@
 package violet.states.menus;
 
-import flixel.FlxCamera;
+import flixel.math.FlxAngle;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
 import violet.backend.options.Options;
 import violet.data.Constants;
 import flixel.FlxObject;
@@ -11,8 +14,6 @@ import violet.backend.utils.MathUtil;
 import violet.backend.utils.NovaUtils;
 import violet.backend.utils.ParseUtil;
 import violet.backend.scripting.events.SelectionEvent;
-
-import thx.semver.Version;
 
 #if debug
 import violet.backend.display.DebugDisplay;
@@ -46,10 +47,30 @@ typedef MenuData = {
 	var items:Array<MenuItem>;
 }
 
+enum abstract NovaMenuAlignment(String) from String {
+	var LEFT = 'left';
+	var RIGHT = 'right';
+
+	@:from public static function fromFlxTextAlign(value:FlxTextAlign):NovaMenuAlignment {
+		return switch (value) {
+			case FlxTextAlign.LEFT: LEFT;
+			case FlxTextAlign.RIGHT: RIGHT;
+			default: LEFT;
+		}
+	}
+	@:to public function toFlxTextAlign():FlxTextAlign {
+		return switch (abstract) {
+			case LEFT: FlxTextAlign.LEFT;
+			case RIGHT: FlxTextAlign.RIGHT;
+			default: FlxTextAlign.LEFT;
+		}
+	}
+}
+
 class MainMenu extends StateBackend {
 
 	public var watermarkTexts = [
-		Constants.ENGINE_TITLE + ' v' + Constants.ENGINE_VERSION + (Constants.ENGINE_SUFFIX != '' ? '-${Constants.ENGINE_SUFFIX}' : '')
+		'${Constants.ENGINE_TITLE} v${Constants.ENGINE_VERSION + (Constants.ENGINE_SUFFIX != '' ? '-${Constants.ENGINE_SUFFIX}' : '')}'
 	];
 
 	public var debugTexts = [
@@ -57,12 +78,9 @@ class MainMenu extends StateBackend {
 		'Branch: ${Constants.GITHUB_BRANCH}'
 	];
 
-	public var curSelectedString:String = '';
-	public var bgColorString:String = '';
-
 	public var reloadingText:NovaText;
 	public var reloadingBG:NovaSprite;
-	// public var reloadingCamera:FlxCamera;
+
 	public static var fromReload:Bool = false;
 
 	public static var curSelected:Int = 0;
@@ -72,10 +90,10 @@ class MainMenu extends StateBackend {
 	public var bg:NovaSprite;
 
 	public var menuItems:Array<NovaSprite> = [];
+	public var itemsGroup:FlxTypedGroup<NovaSprite>;
 
-	public var baseYs:Array<Float> = [];
-	public var floatOffsets:Array<Float> = [];
-	public var menuTime:Float = 0;
+	public var itemRadius(default, never):FlxPoint = new FlxPoint(350, 350);
+	public var itemRadiusPosOffset:Float = -90;
 
 	public var enableMobileControls:Bool = #if mobile true #else false #end;
 
@@ -86,11 +104,41 @@ class MainMenu extends StateBackend {
 
 	public var substateTrans:Bool = true;
 
-	public var menuAlignment:String = 'center';
-	public var watermarkAlignment:String = 'right';
+	public var menuAlignment(default, set):NovaMenuAlignment = LEFT;
+	inline function set_menuAlignment(value:NovaMenuAlignment):NovaMenuAlignment {
+		if (flower != null) {
+			switch (value) {
+				case LEFT: flower.x = -flower.width / 2;
+				case RIGHT: flower.x = FlxG.width - (flower.width / 2);
+			}
+		}
+		switch (value) {
+			case LEFT: itemRadius.x = Math.abs(itemRadius.x);
+			case RIGHT: itemRadius.x = Math.abs(itemRadius.x) * -1;
+		}
+		return menuAlignment = value;
+	}
+	public var watermarkAlignment(default, set):NovaMenuAlignment = RIGHT;
+	inline function set_watermarkAlignment(value:NovaMenuAlignment):NovaMenuAlignment {
+		if (leftWatermark != null) {
+			leftWatermark.alignment = value;
+			switch (value) {
+				case LEFT: leftWatermark.x = 10;
+				case RIGHT: leftWatermark.x = FlxG.width - leftWatermark.getWidth() - 5;
+			}
+		}
+		if (debugWatermark != null) {
+			debugWatermark.alignment = value;
+			switch (value) {
+				case LEFT: debugWatermark.x = 10;
+				case RIGHT: debugWatermark.x = FlxG.width - debugWatermark.getWidth();
+			}
+		}
+		return watermarkAlignment = value;
+	}
 
 	public var flower:NovaSprite;
-	public var flowerTargetAngle:Float = 0;
+	@:unreflective var flowerRotation:Float = 0;
 
 	public static var instance:MainMenu;
 
@@ -110,110 +158,76 @@ class MainMenu extends StateBackend {
 
 		NovaUtils.playMenuMusic();
 
-		var modMenu:violet.states.menus.ModMenu = new violet.states.menus.ModMenu();
-
-		// FlxG.camera.color = FlxColor.BLACK;
-
 		menuData = ParseUtil.json('data/config/menuData');
-		for (i in menuData.items)
-			i.transition ??= true;
-
-		#if mobile
 		for (i in menuData.items) {
-			if (i.id == 'mods') menuData.items.remove(i);
+			i.transition ??= true;
+			i.animations.offsets ??= {idle: [0, 0], selected: [0, 0]};
+			i.animations.offsets.idle ??= [0, 0];
+			i.animations.offsets.selected ??= [0, 0];
+			#if mobile if (i.id == 'mods') menuData.items.remove(i); #end
 		}
-		#end
 
-		var mult:Float = 1/(menuData.items.length);
-		bg = new NovaSprite(Paths.image(menuData.directory + '/' + menuData.background));
+		bg = new NovaSprite(Paths.image('${menuData.directory}/${menuData.background}'));
 		bg.setGraphicSize(FlxG.width, FlxG.height);
-		bg.scale.x += mult;
-		bg.scale.y += mult;
 		bg.updateHitbox();
-		bg.screenCenter();
+		bg.scrollFactor.set();
 		bg.color = menuData.items[curSelected].color;
-		bg.scrollFactor.set(0, mult/2);
-		bg.x = 0;
 		add(bg);
 
-		var overlay:NovaSprite = new NovaSprite(0, 0);
+		var overlay:NovaSprite = new NovaSprite();
 		overlay.setGraphicSize(FlxG.width, FlxG.height);
 		overlay.color = FlxColor.BLACK;
 		overlay.alpha = 0.25;
 		overlay.scrollFactor.set();
 		add(overlay);
 
-		flower = new NovaSprite(0, 0, Paths.image(menuData.directory + '/violet'));
+		flower = new NovaSprite(Paths.image('${menuData.directory}/violet'));
 		flower.screenCenter(Y);
-		flower.scrollFactor.set(1, 0);
-		flower.updateHitbox();
-		flower.centerOrigin();
-		flower.x = FlxG.width - flower.width / 2;
-		// add(flower);
+		flower.scrollFactor.set(1);
+		add(flower);
 
+		itemsGroup = new FlxTypedGroup<NovaSprite>();
 		for (i=>daItem in menuData.items) {
 			var startY = (175*i)+90;
-			var item = new NovaSprite(FlxG.width/2, startY, Paths.image(menuData.directory + '/' + daItem.item));
-			item.addAnim('selected', daItem.item + ' ' + daItem.animations.selected, [], daItem.animations?.offsets?.selected ?? [0, 0], 24, true);
-			item.addAnim('static', daItem.item + ' ' + daItem.animations.idle, [], daItem.animations?.offsets?.idle ?? [0, 0], 24, true);
+			var item = new NovaSprite(FlxG.width/2, startY, Paths.image('${menuData.directory}/${daItem.item}'));
+			item.addAnim('selected', '${daItem.item} ${daItem.animations.selected}', [], daItem.animations?.offsets?.selected ?? [0, 0], 24, true);
+			item.addAnim('static', '${daItem.item} ${daItem.animations.idle}', [], daItem.animations?.offsets?.idle ?? [0, 0], 24, true);
 			item.playAnim('static');
-			item.scale.set(daItem.scale ?? 1, daItem.scale ?? 1);
+			item.scale.scale(daItem.scale ?? 1);
 			item.updateHitbox();
-			item.centerOrigin();
 			item.centerOffsets();
-			baseYs.push(startY);
-			floatOffsets.push(Math.random() * Math.PI * 2);
 			if (daItem.disabled) item.color = FlxColor.interpolate(item.color, FlxColor.BLACK, 0.25);
-			//item.screenCenter(X);
-			menuItems.push(item);
-			add(item);
+			menuItems.push(itemsGroup.add(item));
 		}
+		add(itemsGroup);
 
-		leftWatermark = new NovaText(10, 0, 'Nova Engine v0.1', 20);
+		leftWatermark = new NovaText(10, 'Nova Engine v0.1', 20);
 		leftWatermark.y = FlxG.height - leftWatermark.getHeight() - 5;
 		leftWatermark.setFormat(Paths.font('vcr.ttf'), 40);
 		leftWatermark.scrollFactor.set();
-		leftWatermark.alignment = watermarkAlignment;
-		switch (watermarkAlignment) {
-			case 'right':
-				leftWatermark.x = FlxG.width - leftWatermark.getWidth();
-			default:
-				leftWatermark.x = 10;
-		}
-		//leftWatermark.setFormat(Paths.font('Tardling v1.1.ttf'), 20);
+		leftWatermark.borderStyle = OUTLINE;
+		leftWatermark.borderColor = FlxColor.BLACK;
+		leftWatermark.borderSize = 3;
+		leftWatermark.updateHitbox();
 		add(leftWatermark);
 
 		debugWatermark = new NovaText(10, 10, debugTexts.join('\n'), 20);
 		debugWatermark.setFormat(Paths.font('vcr.ttf'), 40);
 		debugWatermark.scrollFactor.set();
-		debugWatermark.alignment = watermarkAlignment;
-		switch (watermarkAlignment) {
-			case 'right':
-				debugWatermark.x = FlxG.width - debugWatermark.getWidth();
-			default:
-				debugWatermark.x = 10;
-		}
-		//leftWatermark.setFormat(Paths.font('Tardling v1.1.ttf'), 20);
+		debugWatermark.borderStyle = OUTLINE;
+		debugWatermark.borderColor = FlxColor.BLACK;
+		debugWatermark.borderSize = 3;
+		debugWatermark.updateHitbox();
 		add(debugWatermark);
 
-
-		#if FLX_DEBUG
-		FlxG.watch.add(this, 'curSelectedString', 'Current Selected Item:');
-		FlxG.watch.add(this, 'bgColorString', 'Background Color:');
-		FlxG.game.debugger.console.registerFunction('setSelectionColor', setSelectionColor);
-		FlxG.game.debugger.console.registerFunction('changeSelection', changeSelection);
-		#end
-
 		changeSelection(uiCheck());
-		FlxG.camera.snapToTarget();
-		FlxG.camera.followLerp = 0.1;
 
 		if (fadeIn) FlxG.camera.fade(FlxColor.BLACK, 0.25, true);
 
 		#if debug
 		DebugDisplay.registerVariable('Current Menu Item Index', () -> return curSelected);
-		DebugDisplay.registerVariable('Current Menu Item', () -> return curSelectedString);
-		DebugDisplay.registerVariable('Background Color', () -> return bgColorString);
+		DebugDisplay.registerVariable('Current Menu Item', () -> return menuData.items[curSelected].item);
+		DebugDisplay.registerVariable('Background Color', () -> return bg.color.toWebString());
 		DebugDisplay.registerVariable('Can Select', () -> return canSelect);
 		#end
 
@@ -223,7 +237,7 @@ class MainMenu extends StateBackend {
 		reloadingBG.scrollFactor.set();
 		add(reloadingBG);
 
-		reloadingText = new NovaText(0, 0, 0, 'Reloading Mods...', Paths.font('Tardling v1.1'));
+		reloadingText = new NovaText('Reloading Mods...', Paths.font('Tardling v1.1'));
 		reloadingText.size = 100;
 		reloadingText.x = 20;
 		reloadingText.y = FlxG.height - 50;
@@ -234,27 +248,28 @@ class MainMenu extends StateBackend {
 		reloadingBG.alpha = fromReload ? 1 : 0;
 		reloadingText.alpha = fromReload ? 1 : 0;
 
-		if (fromReload) FlxTween.tween(reloadingBG, { alpha: 0 }, 0.5);
-		if (fromReload) FlxTween.tween(reloadingText, { alpha: 0 }, 0.5);
+		if (fromReload) {
+			fromReload = false;
 
-		if (fromReload) fromReload = false;
+			FlxTween.tween(reloadingBG, { alpha: 0 }, 0.5);
+			FlxTween.tween(reloadingText, { alpha: 0 }, 0.5);
 
-		// if (fromReload) FlxTimer.wait(0.1, ()->FlxTween.tween(reloadingCamera, { alpha: 0 }, 1, { ease: FlxEase.expoOut }));
-		// if (fromReload) fromReload = false;
+			// FlxTimer.wait(0.1, ()->FlxTween.tween(reloadingCamera, { alpha: 0 }, 1, { ease: FlxEase.expoOut }));
+			// fromReload = false;
+		}
 
 		callInScripts('postCreate');
 	}
 
-	function uiCheck() {
+	inline function uiCheck() {
 		if (!canSelect) return 0;
-		if (Controls.uiUp)
-			return -1;
-		else if (Controls.uiDown)
-			return 1;
-		else
-			return 0;
+		if (Controls.uiUp) return -1;
+		else if (Controls.uiDown) return 1;
+		else return 0;
 	}
 
+	@:unreflective final _flower_midpoint:FlxPoint = FlxPoint.get();
+	@:unreflective final _cur_cat_pos:FlxPoint = FlxPoint.get();
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
@@ -276,49 +291,44 @@ class MainMenu extends StateBackend {
 		if (canSelect && (Controls.uiUp || Controls.uiDown))
 			changeSelection(uiCheck());
 		bg.color = MathUtil.colorLerp(bg.color, menuData.items[curSelected].color, 0.16);
-		bgColorString = ParseColor.fromInt(bg.color);
-
-		leftWatermark.updateHitbox();
-		switch (watermarkAlignment) {
-			case 'right':
-				leftWatermark.x = FlxG.width - leftWatermark.getWidth() - 5;
-			default:
-				leftWatermark.x = 10;
-		}
 
 		debugWatermark.visible = Options.data.developerMode;
-		debugWatermark.updateHitbox();
-		switch (watermarkAlignment) {
-			case 'right':
-				debugWatermark.x = FlxG.width - debugWatermark.getWidth() - 5;
-			default:
-				debugWatermark.x = 10;
-		}
 
 		if (Controls.accept) {
 			pickSelection();
 		}
 
-		menuTime += elapsed;
-		for (i => item in menuItems) {
-			var bob = Math.sin(menuTime * 1.6 + floatOffsets[i]) * 6;
-			item.y = baseYs[i] + bob;
-			var configured = menuData.items[i].scale ?? 1;
-			var target = (i == curSelected) ? configured * 1.12 : configured;
-			item.scale.x = FlxMath.lerp(item.scale.x, target, 0.12);
-			item.scale.y = item.scale.x;
+		var flowerTargetAngle = flowerRotation + Math.PI / 2 + FlxAngle.TO_RAD * itemRadiusPosOffset;
+		flower.angle = lerp(flower.angle, -flowerTargetAngle * FlxAngle.TO_DEG, 0.2);
+
+		for (index => item in menuItems) {
+			var angle = (index / menuItems.length) * Math.PI * 2 - flowerTargetAngle;
+
+			flower.getMidpoint(_flower_midpoint);
+			var position = _cur_cat_pos.set(
+				_flower_midpoint.x + FlxMath.fastCos(angle) * itemRadius.x,
+				_flower_midpoint.y + FlxMath.fastSin(angle) * itemRadius.y
+			);
+			item.setPosition(
+				lerp(item.x, position.x - (item.width / 2), 0.2),
+				lerp(item.y, position.y - (item.height / 2), 0.2)
+			);
+
+			var configured = menuData.items[index].scale ?? 1;
+			var target = (index == curSelected) ? configured * 1.12 : configured;
+			item.scale.x = item.scale.y = lerp(item.scale.x, target * 0.7, 0.2);
+
+			item.zIndex = Std.int(((FlxMath.fastSin(angle - flowerTargetAngle) + 1) / 2) * 1000);
 		}
 
-		flower.angle = FlxMath.lerp(flower.angle, flowerTargetAngle, Math.min(1, elapsed * 8));
 
 		if (Controls.back) {
 			// Main.switchState(new ClassData('TitleState')); // Crashes idk why
 		}
 
 		var instance = watermarkTexts.copy();
-		for (i in ModdingAPI.getActiveMods()) {
+		for (i in ModdingAPI.getActiveMods())
 			instance.push('${i.title} v${i?.mod_version}');
-		}
 
 		instance.sort(function(a, b):Int {
 			if(a.length < b.length) return -1;
@@ -326,23 +336,14 @@ class MainMenu extends StateBackend {
 			else return 0;
 		});
 
-
-		leftWatermark.borderStyle = OUTLINE;
-		leftWatermark.borderColor = FlxColor.BLACK;
-		leftWatermark.borderSize = 3;
 		leftWatermark.text = instance.join('\n');
 		leftWatermark.updateHitbox();
 		if (canSelect) {
 			leftWatermark.y = FlxG.height - leftWatermark.getHeight() - 5;
-		}
-
-		debugWatermark.borderStyle = OUTLINE;
-		debugWatermark.borderColor = FlxColor.BLACK;
-		debugWatermark.borderSize = 3;
-		debugWatermark.updateHitbox();
-		if (canSelect) {
 			debugWatermark.y = 10;
 		}
+		instance.resize(0);
+
 
 		if (FlxG.keys.justPressed.SEVEN && Options.data.developerMode) {
 			substateTrans = false;
@@ -353,20 +354,19 @@ class MainMenu extends StateBackend {
 		if (canSelect) {
 			for (i => item in menuItems) {
 				if (FlxG.mouse.overlaps(item) && FlxG.mouse.justPressed) {
-					if (curSelected == i) {
-						pickSelection();
-					} else {
-						changeSelection(i-curSelected);
-					}
+					if (curSelected == i) pickSelection();
+					else changeSelection(i - curSelected);
 				}
 			}
 		}
 	}
 
 	public function changeSelection(amt:Int) {
+		var totalAmt = amt;
 		var target = FlxMath.wrap(curSelected + amt, 0, menuItems.length-1);
 		while (menuData.items[target].disabled) {
 			target = FlxMath.wrap(target + amt, 0, menuItems.length-1);
+			totalAmt += amt;
 		}
 		var event:SelectionEvent = new SelectionEvent(target);
 		if (amt != 0) {
@@ -378,32 +378,16 @@ class MainMenu extends StateBackend {
 		}
 		curSelected = event.selection;
 		for (i => item in menuItems) {
-			if (i == curSelected) {
-				item.updateHitbox();
-				var daTarget:FlxObject = new FlxObject();
-				daTarget.x = (FlxG.width/2);
-				daTarget.y = item.y + (item.height/2);
-				FlxG.camera.target = daTarget;
-			}
 			item.playAnim(i == curSelected ? 'selected' : 'static');
 			item.updateHitbox();
-			if (menuAlignment != 'center') { // looked weird with the offset animations, so might as well not apply them for centered menus
-				item.offset.x = item.anims.get(i == curSelected ? 'selected' : 'static').offset[0];
-				item.offset.y = item.anims.get(i == curSelected ? 'selected' : 'static').offset[1];
-			}
 			if (canSelect) {
-				switch (menuAlignment) {
-					case 'center':
-						item.screenCenter(X);
-					case 'left':
-						item.x = 50;
-					case 'right':
-						item.x = FlxG.width - item.width - 50;
-				}
+				menuAlignment = menuAlignment;
+				watermarkAlignment = watermarkAlignment;
 			}
 		}
-		curSelectedString = menuData.items[curSelected].item;
-		flowerTargetAngle += -amt * 360 / 6;
+
+		var step = (Math.PI * 2) / menuItems.length;
+		flowerRotation += step * totalAmt;
 	}
 
 	public function pickSelection() {
@@ -436,10 +420,6 @@ class MainMenu extends StateBackend {
 		});
 	}
 
-	public function setSelectionColor(hex) {
-		menuData.items[curSelected].color = hex;
-	}
-
 	override function closeSubState() {
 		super.closeSubState();
 		if (!substateTrans) {
@@ -449,12 +429,8 @@ class MainMenu extends StateBackend {
 		for (i in menuItems) {
 			var prev = i.x;
 			switch (menuAlignment) {
-				case 'center':
-					i.screenCenter(X);
-				case 'left':
-					i.x = 50;
-				case 'right':
-					i.x = FlxG.width - i.width - 50;
+				case LEFT: i.x = 50;
+				case RIGHT: i.x = FlxG.width - i.width - 50;
 			}
 			FlxTween.tween(i, { x: i.x }, 0.5, { ease: FlxEase.smootherStepOut, onComplete: (_)-> canSelect = true });
 			i.x = prev;
@@ -466,7 +442,5 @@ class MainMenu extends StateBackend {
 	public function reloadMods() {
 		doReload = true;
 	}
-
-
 
 }
