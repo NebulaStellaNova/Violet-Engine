c8744a5d5dc0d42b124a7c9b1c666e66fa9b69f2
💜 Rodney, An Imaginative Furball 💙 <okmilkrb@gmail.com>
2026-05-31 22:31:31 -0400

    minor things

--- source/violet/states/PlayState.hx
+++ source/violet/states/PlayState.hx
@@ -256,7 +256,6 @@ class PlayState extends violet.backend.StateBackend {
 		Conductor.setupBPMChanges(songData, sortedEvents);
 		Conductor.offset = (countdownLength) * Conductor.beatLengthMs;
 
-		trace(Paths.vocal(songData.songName, null, PlayState.variation));
 		if (Paths.vocal(songData.songName, null, PlayState.variation) != '') generalVocals = Conductor.addAdditionalTrack(FlxG.sound.load(Cache.sound(Paths.vocal(songData.songName, null, PlayState.variation), 'root', null, true), FlxG.sound.defaultMusicGroup));
 		else generalVocals = Conductor.addAdditionalTrack(new FlxSound());
 
